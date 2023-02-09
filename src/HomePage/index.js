e61e5c0813935b67b1136def2f39756a14511dbf
TorjusL <60695392+TorjusL@users.noreply.github.com>
2023-02-09 16:00:28 +0100

    yo

--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome gdlfkmdsfdsfsdfdsfdsfdsfdsfdsfdsfdsfdsfdslk yo</h1>
+      <h1>Wefdsfdslk yo</h1>
     </div>
   )
 }
