8d1abfe82c56e110fd698715a0344d08ba0f485f
wavebreker <112630282+Alador22@users.noreply.github.com>
2023-04-26 17:25:23 +0200

    nav update

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,21 +1,9 @@
-import React, { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../AuthContext";
-import './style.css';
+import "./style.css";
 
-<<<<<<< HEAD
 function Navbar({ isLoggedIn, setIsLoggedIn }) {
-=======
-function Navbar() {
-  const { isLoggedIn, handleLogout } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleLogoutClick = () => {
-    handleLogout();
-    navigate("/");
-  };
-
->>>>>>> 54efbe36ebc3fb741f5af893a6d3081c1ea873c3
   return (
     <nav>
       <ul>
@@ -29,22 +17,19 @@ function Navbar() {
           <Link to="/FindMatches">Find Matches</Link>
         </li>
       </ul>
-<<<<<<< HEAD
-      
-      <Link to="/"><button onClick={() => {
-      if (isLoggedIn) {
-        setIsLoggedIn(false);
-      }
-    }}
-  >
-    {isLoggedIn ? "Log Out" : "Log In"}
-  </button>
-</Link>
-=======
-      <button onClick={handleLogoutClick}>{isLoggedIn ? 'Logg ut' : 'Logg inn'}</button>
->>>>>>> 54efbe36ebc3fb741f5af893a6d3081c1ea873c3
+
+      <Link to="/">
+        <button
+          onClick={() => {
+            if (isLoggedIn) {
+              setIsLoggedIn(false);
+            }
+          }}
+        >
+          {isLoggedIn ? "Log Out" : "Log In"}
+        </button>
+      </Link>
     </nav>
-    
   );
 }
 
