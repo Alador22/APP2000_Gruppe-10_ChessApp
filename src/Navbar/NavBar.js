4be08754411f2b1f44df69f9b6d2ec99cb9fb9eb
JamesCarter216810 <216810@usn.no>
2023-05-21 12:59:52 +0200

    La til Admin side

--- src/Navbar/NavBar.js
+++ src/Navbar/NavBar.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, {useContext} from "react";
 import { NavLink } from "react-router-dom";
 import "./style.css";
 import DropdownMeny from "./dropdownmeny"
+import { AuthContext } from "../AuthContext";
 
 function Navbar({ isLoggedIn, setIsLoggedIn }) {
   
+  const { authData } = useContext(AuthContext);
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to="/" exact >Home</NavLink>
+          <NavLink to="/" exact >Hjem</NavLink>
         </li>
         <li>
-          <NavLink to="/Practice">Practice</NavLink>
+          <NavLink to="/Practice">Øving</NavLink>
         </li>
         <li>
-          <NavLink to="/FindMatches">Find Matches</NavLink>
+          <NavLink to="/FindMatches">Profilside</NavLink>
+        </li>
+        <li>
+          <NavLink to="/AdminPage">Administrator</NavLink>
         </li>
       </ul>
+      {isLoggedIn && (
+        <p className="logged-in-text">
+        Logget inn som: {authData.email}
+      </p>
+      )}
       <DropdownMeny isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
     </nav>
   );
