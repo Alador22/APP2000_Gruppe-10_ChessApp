73f59a0819ece91746190a5591fee51626559077
wavebreker <112630282+Alador22@users.noreply.github.com>
2023-05-30 01:53:27 +0200

    jwt small patch

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect } from 'react';
-import jwtDecode from 'jwt-decode';
+import React, { createContext, useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
 
 export const AuthContext = createContext();
 
@@ -7,15 +7,14 @@ export const AuthProvider = ({ children }) => {
   const [authData, setAuthData] = useState(null);
   const [admin, setAdmin] = useState(false);
 
-
-  // Laget av Jørgen og ChatGPT 
+  // Laget av Jørgen og ChatGPT
   // Decoder JWT og henter ut informasjon
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      //const decodedToken = jwtDecode(token);
-      setAuthData(token);
-      setAdmin(token.admin); // Gjør at token blir admin om den har disse rettighetene fra før
+      const decodedToken = jwtDecode(token);
+      setAuthData(decodedToken);
+      setAdmin(decodedToken.admin); // Gjør at token blir admin om den har disse rettighetene fra før
     }
   }, []);
 
